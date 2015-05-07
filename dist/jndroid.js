4caedb9d852b1e84ba7a2616648780a66577df16
greentea <yanglf2@lenovo.com>
2015-05-07 14:07:08 +0800

    robot compress

--- dist/jndroid.js
+++ dist/jndroid.js
@@ -910,7 +910,7 @@ function View() {
         this.getDiv().style.borderBottom = thick + "px solid " + Utils.toCssColor(color);
     };
 
-    this.setBoxShadow = function(x, y, blur, spread) {
+    this.setBoxShadow = function(x, y, blur, spread, color) {
 
         var styleString = x + 'px ' + y + 'px';
 
@@ -922,6 +922,11 @@ function View() {
         if(typeof(spread) != 'undefined')
         {
             styleString = styleString + ' ' + spread + 'px';
+        }
+
+        if(typeof(color) != 'undefined')
+        {
+            styleString = styleString + ' ' + Utils.toCssColor(color);
         }
 
         this.getDiv().style.webkitBoxShadow =  styleString;
