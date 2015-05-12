38babf61513d34e94c09cf444a7f7923f3495748
guyiyang <guyy1@lenovo.com>
2015-05-12 09:10:11 +0800

    ImageView和WebView的onMeasure改为MeasureSpec传递尺寸

--- src/jndroid.widget.js
+++ src/jndroid.widget.js
@@ -145,7 +145,9 @@ function ImageView() {
         mCustom = true;
     };
 
-    this.onMeasure = function (width, height) {
+    this.onMeasure = function (widthMS, heightMS) {
+        var width = MeasureSpec.getSize(widthMS);
+        var height = MeasureSpec.getSize(heightMS);
         this.getDiv().style.lineHeight = height + "px";
 
         this.setMeasuredDimension(width, height);
@@ -476,7 +478,9 @@ function WebView() {
         return mFrame;
     };
 
-    this.onMeasure = function(width, height) {
+    this.onMeasure = function(widthMS, heightMS) {
+        var width = MeasureSpec.getSize(widthMS);
+        var height = MeasureSpec.getSize(heightMS);
         mFrame.style.width = width + "px";
         mFrame.style.height = height + "px";
         this.setMeasuredDimension(width, height);
