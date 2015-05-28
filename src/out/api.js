1c22aa7bc2a84978d968237f0ae09f0412ab903c
jzyang <jizhongyang@foxmail.com>
2015-05-28 10:13:17 +0800

    更新资源文件

--- src/out/api.js
+++ src/out/api.js
@@ -1,16 +1,13 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
-        "Button",
         "Drawable",
         "FrameLayout",
         "Gallery",
-        "ImageView",
         "LayoutParams",
         "LinearLayout",
         "MeasureSpec",
         "MotionEvent",
-        "ScrollView",
         "View"
     ],
     "modules": [],
