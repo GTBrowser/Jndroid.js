97c7d235bb5b14e8c572821ddc594620a4d2e974
jzyang <jizhongyang@foxmail.com>
2015-05-29 16:05:16 +0800

    更新jndroid.widget.js的相关资源文件

--- src/out/api.js
+++ src/out/api.js
@@ -1,14 +1,22 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "Button",
         "Drawable",
+        "EditText",
         "FrameLayout",
         "Gallery",
+        "HorizontalScrollView",
+        "ImageButton",
+        "ImageView",
         "LayoutParams",
         "LinearLayout",
         "MeasureSpec",
         "MotionEvent",
-        "View"
+        "ScrollView",
+        "TextView",
+        "View",
+        "WebView"
     ],
     "modules": [],
     "allModules": []
