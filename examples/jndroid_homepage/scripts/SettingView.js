4dfd2f1e2ad382d1a945eb5d44d7352c18753b44
guyiyang <guyy1@lenovo.com>
2015-10-29 16:31:46 +0800

    设置调整

--- examples/jndroid_homepage/scripts/SettingView.js
+++ examples/jndroid_homepage/scripts/SettingView.js
@@ -1,46 +1,38 @@
 function SettingView() {
-    ViewGroup.apply(this, []);
+    ViewGroup.apply(this);
 
     this.setBackgroundColor(0xffffffff);
     this.setBoxShadow(0, 0, 16, 0, R.color.shadow);
 
-    var mContent = new LinearLayout();
-    this.addView(mContent);
+    var cnt = new LinearLayout();
+    this.addView(cnt);
 
-    var mTitle = new TextView();
-    mTitle.setText(R.string.setting);
-    mTitle.setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL);
-    mTitle.setTextSize(16);
-    mTitle.setTextColor(R.color.text);
-    mTitle.setPadding(R.dimen.padding);
-    mTitle.setBorderBottom(1, R.color.dividers);
+    var title = Theme.createTitle(R.string.setting);
+    title.setPadding(R.dimen.padding);
+    title.setBorderBottom(1, R.color.dividers);
     var titleLp = new LayoutParams(LayoutParams.FILL_PARENT, 48);
     titleLp.topMargin = 48;
-    mContent.addView(mTitle, titleLp);
+    cnt.addView(title, titleLp);
 
-    var mLanguageTitle = new TextView();
-    mLanguageTitle.setText(R.string.language);
-    mLanguageTitle.setTextSize(14);
-    mLanguageTitle.setTextColor(R.color.sub_text);
-    mLanguageTitle.setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL);
-    mLanguageTitle.setPadding(R.dimen.padding);
+    var languageTitle = Theme.createText(R.string.language);
+    languageTitle.setPadding(R.dimen.padding);
     var subTitleLp = new LayoutParams(LayoutParams.FILL_PARENT, 48);
-    mContent.addView(mLanguageTitle, subTitleLp);
+    cnt.addView(languageTitle, subTitleLp);
 
-    var mZhRadioButton = new MRadioButton();
-    mZhRadioButton.setText("中文");
-    mZhRadioButton.setId(0);
-    mZhRadioButton.setColor(R.color.theme);
+    var zhRadioButton = new MRadioButton();
+    zhRadioButton.setText("中文");
+    zhRadioButton.setId(0);
+    zhRadioButton.setColor(R.color.theme);
 
-    var mEnRadioButton = new MRadioButton();
-    mEnRadioButton.setText("English");
-    mEnRadioButton.setId(1);
-    mEnRadioButton.setColor(R.color.theme);
+    var enRadioButton = new MRadioButton();
+    enRadioButton.setText("English");
+    enRadioButton.setId(1);
+    enRadioButton.setColor(R.color.theme);
 
-    var mLanguageGroup = new MRadioGroup();
-    mLanguageGroup.addChild(mZhRadioButton);
-    mLanguageGroup.addChild(mEnRadioButton);
-    mLanguageGroup.setOnCheckedChangeListener(function(id) {
+    var languageGroup = new MRadioGroup();
+    languageGroup.addChild(zhRadioButton);
+    languageGroup.addChild(enRadioButton);
+    languageGroup.setOnCheckedChangeListener(function(id) {
         if (id == 0) {
             localStorage["language"] = "zh";
         } else {
@@ -50,30 +42,27 @@ function SettingView() {
     });
     var languageLp = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
     languageLp.leftMargin = R.dimen.padding;
-    mContent.addView(mLanguageGroup, languageLp);
+    cnt.addView(languageGroup, languageLp);
 
     if (Manifest.language.indexOf("zh") >= 0) {
-        mZhRadioButton.setChecked(true);
+        zhRadioButton.setChecked(true);
     } else {
-        mEnRadioButton.setChecked(true);
+        enRadioButton.setChecked(true);
     }
 
-    this.onMeasure = function(widthMS, heightMS) {
-        var width = MeasureSpec.getSize(widthMS) * 2 / 3;
-        var height = MeasureSpec.getSize(heightMS);
+    this.onMeasure = function(wMS, hMS) {
+        var w = MeasureSpec.getSize(wMS) * 2 / 3;
+        var h = MeasureSpec.getSize(hMS);
 
-        if (width > 300) {
-            width = 300;
+        if (w > 300) {
+            w = 300;
         }
+        cnt.measure(w, h);
 
-        mContent.measure(width, height);
-
-        this.setMeasuredDimension(width, height);
+        this.setMeasuredDimension(w, h);
     };
 
-    this.onLayout = function(x, y) {
-        var offsetX = 0;
-        var offsetY = 0;
-        mContent.layout(offsetX, offsetY);
+    this.onLayout = function() {
+        cnt.layout(0, 0);
     }
 }
