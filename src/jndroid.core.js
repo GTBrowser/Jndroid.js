e2f1870395a664f71980ab797a58a183ea07515a
guyiyang <guyy1@lenovo.com>
2015-11-05 10:45:39 +0800

    [online]更新统计代码

--- src/jndroid.core.js
+++ src/jndroid.core.js
@@ -2475,7 +2475,7 @@ function touch(e) {
     }
 }
 //
-/* statistics code start, you can replace to your own code. www.clicki.cn is good to use.*/
+/* statistics code start, you can replace to your own code*/
 var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
 document.write(unescape("%3Cspan id='cnzz_stat_icon_1256652931'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1256652931' type='text/javascript'%3E%3C/script%3E"));
 ///* statistics code end */
